d67cff3d145bbb1c4a0913c55dfb4fcf9824ea03
Wesley <weslcld@gmail.com>
2020-06-16 19:45:19 -0300

    Updates

---
+++ precache-manifest.89821003dfc1503706cbf1130a87b476.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82f0992a0e789da6e13725787b572e8b",
+    "url": "/react-youtube/index.html"
+  },
+  {
+    "revision": "d8bf89f2ac2bb86c5982",
+    "url": "/react-youtube/static/css/main.ced9543a.chunk.css"
+  },
+  {
+    "revision": "9a747e3e2c221b363afc",
+    "url": "/react-youtube/static/js/2.4f5c84f4.chunk.js"
+  },
+  {
+    "revision": "967f74ec7a55679f03c335e2cb871d77",
+    "url": "/react-youtube/static/js/2.4f5c84f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8bf89f2ac2bb86c5982",
+    "url": "/react-youtube/static/js/main.aca9ca1a.chunk.js"
+  },
+  {
+    "revision": "6ce0ba2bfb664daf9b82",
+    "url": "/react-youtube/static/js/runtime-main.72b6c21f.js"
+  }
+]);
